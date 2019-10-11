e9f659849cd4a83774c66da40365fadee7907924
demilly baptiste <demillybaptiste@gmail.com>
2019-10-11 23:21:03 +0200

    wip json for sqlspark

--- dataframe_spark_SQL.scala
+++ dataframe_spark_SQL.scala
@@ -1,4 +1,6 @@
 package ScalaSpells
+import java.io.File
+
 import org.apache.spark
 import org.apache.spark.sql.DataFrameReader
 import org.apache.spark.sql.DataFrame
@@ -21,7 +23,16 @@ object dataframe_spark_SQL extends App {
   override def main(args: Array[String]) {
     val path = "spells_thread12.txt"
     val textFile=Source.fromFile(path).getLines
-    var dataset=textFile.flatMap(line => "[\""+line.split(";")(0)+"\",\""+line.split(";")(1)+"\",\""+line.split(";")(2)+"\","+line.split(";")(3)+"]")
+    var dataset=textFile.flatMap(line => "{\"name\":\""+line.split(";")(0)+"\",\"level\": \""+line.split(";")(1)+"\",\"composant\":\""+line.split(";")(2)+"\",\"spellresist\":"+line.split(";")(3)+"}"+"\n")
+    val fileName = "spells_thread.json"
+
+    val file_to_check = new File(fileName)
+    val exist = file_to_check.exists()
+    if (exist) {
+      file_to_check.delete()
+    }
+
+
     dataset.foreach(line =>print (line))
 
   }
